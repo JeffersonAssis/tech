7ab2650ee493a5bc7381831fe7db28244e0a29e2
Jefferson Assis <jbrunoassis@gmail.com>
2024-02-12 22:03:57 -0300

    aaaa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { ContatoComponent } from './contato/contato.component';
 
 export const routes: Routes = [
-  {path:'contato', component:ContatoComponent},
+  {path:'contato',component:ContatoComponent},
   {path:'carrinho', component:CarrinhoComponent},
   {path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   {path:'', redirectTo: 'produtos', pathMatch:'full'},
